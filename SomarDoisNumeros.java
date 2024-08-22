57b7d0d5bab3b1b7cb184143f0c2ab0e0e740574
Diego Cheib <diegocheib@gmail.com>
2024-08-22 10:42:45 -0300

    feat: Criado metodo para somar 2 inteiros

---
+++ SomarDoisNumeros.java
@@ -0,0 +1,9 @@
+
+class SomarDoisNumeros{
+    public static void main(String[] args){
+        System.out.println(Soma(10, 20));
+    }
+    public static int Soma(int a, int b){
+        return a + b;
+    }
+}
